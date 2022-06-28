02626151da213cf61f885312c2132f84b9eedc2a
Martin Dubois <mdubois@kms-quebec.com>
2022-06-28 16:57:10 -0400

    front-end initial version

---
+++ front-end/src/pages/Home.js
@@ -0,0 +1,13 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Framework
+// File      front-end/src/pages/Home.js
+
+const Home = () =>
+{
+    return <h1>Home</h1>
+}
+
+export default Home;
---
+++ front-end/src/pages/Layout.js
@@ -0,0 +1,50 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Framework
+// File      front-end/src/pages/Layout.js
+
+import { useEffect, useState } from "react";
+import { Outlet, Link } from "react-router-dom"
+
+const GetBackEndVersion = () =>
+{
+    return fetch( 'http://127.0.0.1/Version' )
+        .then( ( aResponse ) => { return aResponse.json() } )
+        .then( ( aData ) => { return aData.Version } )
+        .catch( ( aError ) => { return 'ERROR' } )
+}
+
+const Layout = () =>
+{
+    const [ sBackEndVersion, SetBackEndVersion ] = useState( '' )
+
+    useEffect( () =>
+    {
+        GetBackEndVersion()
+            .then( ( aVersion ) => { SetBackEndVersion( aVersion ); } )
+    })
+
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li><Link to="/"     >Home </Link></li>
+                    <li><Link to="/Test0">Test0</Link></li>
+                </ul>
+            </nav>
+
+            <Outlet />
+
+            <footer>
+                <address>
+                    Copyright &copy; 2022 KMS -
+                    Back-end { sBackEndVersion } -
+                    Front-end 0.0.0
+                </address>
+            </footer>
+        </> )
+};
+
+export default Layout;
