e0ea69aeedcf9aaa0d2b4b4adcc033d36ea8cff0
Martin Dubois <mdubois@kms-quebec.com>
2022-09-19 11:51:08 -0400

    Includes/KMS/ScaleAndOffset.h

---
+++ Includes/KMS/ScaleAndOffset.h
@@ -0,0 +1,30 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2022 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Framework
+// File      Includes/KMS/DAQ/AnalogInputConverter.h
+
+#pragma once
+
+namespace KMS
+{
+
+    class ScaleAndOffset
+    {
+
+    public:
+
+        ScaleAndOffset(double aScale, double aOffset = 0.0);
+
+        double Revert(double aIn) const;
+        double Scale (double aIn) const;
+
+    private:
+
+        double mOffset;
+        double mScale;
+
+    };
+
+}
