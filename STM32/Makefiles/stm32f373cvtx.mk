b3403a7d20a9f00763114d6f175c468fe5c2432e
Martin Dubois <mdubois@kms-quebec.com>
2022-11-28 11:03:01 -0500

    0.1.18-stm32f373cvtx

--- STM32/Makefiles/stm32f373cvtx.mk
+++ STM32/Makefiles/stm32f373cvtx.mk
@@ -5,6 +5,6 @@
 # Product   KMS-Framework
 # File      STM32/Makefiles/stm32f373cvtx.mk
 
-include ../../Makefiles/stm32.mk
+include $(KMS_MAKEFILES)/stm32.mk
 
 CFLAGS += -DSTM32F3 -DSTM32F373xC -DSTM32F373VCTx
