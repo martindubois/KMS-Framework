d2d270e4527153c4426d330a9fd416edd2d6a9f4
Martin Dubois <mdubois@kms-quebec.com>
2022-12-05 18:04:18 -0500

    0.1.52-dev Windows

--- Common/Version.h
+++ Common/Version.h
@@ -15,9 +15,9 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  0,1,51,0
-#define VERSION_STR "0.1.51.0"
+#define VERSION_RC  0,1,52,0
+#define VERSION_STR "0.1.52.0"
 
 #ifdef __cplusplus
-    KMS_VERSION("stm32f373vctx");
+    KMS_VERSION("dev");
 #endif
