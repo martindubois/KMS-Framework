45730b94838d3297ec086360e13c0bc840a35840
Martin Dubois <mdubois@kms-quebec.com>
2023-04-11 14:31:44 -0400

    Click::Master

---
+++ Includes/KMS/Click/Master.h
@@ -0,0 +1,103 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2023 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Framework
+// File      Includes/KMS/Click/Master.h
+
+#pragma once
+
+// ===== C++ ================================================================
+#include <list>
+#include <vector>
+
+namespace KMS
+{
+    namespace Click
+    {
+
+        class Master
+        {
+
+        public:
+
+            enum class KeyCode
+            {
+                F1, F10,
+                RETURN,
+                TAB,
+
+                QTY
+            };
+
+            static const unsigned int FLAG_ALT;
+            static const unsigned int FLAG_DESKTOP;
+            static const unsigned int FLAG_NEXT;
+            static const unsigned int FLAG_NO_EXCEPTION;
+            static const unsigned int FLAG_PUSH;
+            static const unsigned int FLAG_RECURSE;
+            static const unsigned int FLAG_RETRY;
+
+            Master();
+
+            virtual ~Master();
+
+            void SetDelay(unsigned int aDelay_ms);
+
+            void Key(KeyCode aKey, unsigned int aFlags = 0);
+            void Key(char    aKey, unsigned int aFlags = 0);
+
+            void Message_Close();
+
+            // aFlags  FLAG_ALT
+            void SystemKey(KeyCode aKey, unsigned int aFlags = 0);
+            void SystemKey(char    aKey, unsigned int aFlags = 0);
+
+            void Text(char aKey);
+            void Text(const char* aText);
+
+            void Window_Pop();
+
+            // aFlags
+            //  FLAG_DESKTOP       Ignored when FLAG_NEXT is present
+            //  FLAG_NEXT          Do not use with FLAG_RECURSE
+            //  FLAG_NO_EXCEPTION
+            //  FLAG_PUSH
+            //  FLAG_RECURSE       Do not use with FLAG_NEXT
+            //  FLAG_RETRY
+            bool Window_Select(const char* aClass, const char* aName, unsigned int aFlags = 0);
+            bool Window_Select(HWND aHandle, unsigned int aFlags = 0);
+
+        private:
+
+            class FlagData
+            {
+
+            public:
+
+                FlagData();
+
+                HWND         mCurrent;
+                bool         mException;
+                HWND         mParent;
+                bool         mRecurse;
+                unsigned int mRetryCount;
+               
+            };
+
+            NO_COPY(Master);
+
+            void Key(unsigned int aMsg, char aKey, unsigned int aFlags);
+
+            void Message_Post(unsigned int aMsg, uint64_t aParamW = 0, uint64_t aParamL = 0);
+
+            void ParseFlags(unsigned int aFlags, FlagData* aOut);
+
+            HWND            mCurrent;
+            unsigned int    mDelay_ms;
+            std::list<HWND> mStack;
+
+        };
+
+    }
+}
---
+++ KMS-A-Test/Click_Master.cpp
@@ -0,0 +1,61 @@
+
+// Author    KMS - Martin Dubois, P. Eng.
+// Copyright (C) 2023 KMS
+// License   http://www.apache.org/licenses/LICENSE-2.0
+// Product   KMS-Framework
+// File      KMS-A-Test/Click_Master.cpp
+
+#include "Component.h"
+
+// ===== Windows ============================================================
+#include <Windows.h>
+
+// ===== Includes ===========================================================
+#include <KMS/Click/Master.h>
+#include <KMS/Proc/Process.h>
+
+using namespace KMS;
+
+KMS_TEST(Click_Master_Base, "Click_Master_Base", "Auto", sTest_Base)
+{
+    Proc::Process lP(File::Folder::NONE, "Notepad.exe");
+
+    lP.Start();
+    lP.Detach();
+
+    Click::Master lCM;
+
+    lCM.SetDelay(10);
+
+    unsigned int lFlags = Click::Master::FLAG_NO_EXCEPTION | Click::Master::FLAG_RETRY;
+    KMS_TEST_ASSERT_RETURN(lCM.Window_Select(NULL, "Untitled - Notepad", lFlags));
+
+    lCM.SystemKey(Click::Master::KeyCode::F10);
+    lCM.Key('F');
+    lCM.Key('O');
+
+    lFlags |= Click::Master::FLAG_DESKTOP | Click::Master::FLAG_PUSH;
+    KMS_TEST_ASSERT_RETURN(lCM.Window_Select(NULL, "Open", lFlags));
+    {
+        lFlags = Click::Master::FLAG_NO_EXCEPTION | Click::Master::FLAG_PUSH;
+        KMS_TEST_ASSERT_RETURN(lCM.Window_Select("ComboBoxEx32", NULL, lFlags));
+        {
+            lFlags = Click::Master::FLAG_NO_EXCEPTION;
+            KMS_TEST_ASSERT_RETURN(lCM.Window_Select("ComboBox", NULL, lFlags));
+            KMS_TEST_ASSERT_RETURN(lCM.Window_Select("Edit"    , NULL, lFlags));
+
+            char lPath[PATH_LENGTH];
+            File::Folder::CURRENT.GetPath("KMS-A-Test\\Tests\\Test4.txt", lPath, sizeof(lPath));
+
+            lCM.Text(lPath);
+        }
+        lCM.Window_Pop();
+        KMS_TEST_ASSERT_RETURN(lCM.Window_Select(NULL, "&Open", lFlags));
+        lCM.SystemKey('O', Click::Master::FLAG_ALT);
+    }
+    lCM.Window_Pop();
+
+    lCM.Key(Click::Master::KeyCode::F10);
+    lCM.Key('F');
+    lCM.Key('x');
+}
