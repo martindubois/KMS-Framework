ae030ad724c1a46a07b22bc6aa692d81fb2fb8f6
Martin Dubois <mdubois@kms-quebec.com>
2023-05-25 16:35:25 -0400

    0.1.68-dev Windows

--- Common/Version.h
+++ Common/Version.h
@@ -15,8 +15,8 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  0,1,67,0
-#define VERSION_STR "0.1.67.0"
+#define VERSION_RC  0,1,68,0
+#define VERSION_STR "0.1.68.0"
 
 #ifdef __cplusplus
     KMS_VERSION("dev");
