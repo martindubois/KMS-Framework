496a305e0ed90f3101c4a874ba4e45188729006b
Martin Dubois <mdubois@kms-quebec.com>
2023-09-06 10:42:18 -0400

    1.0.5 Windows

--- KMS-A/Graph_Histogram.cpp
+++ KMS-A/Graph_Histogram.cpp
@@ -5,6 +5,8 @@
 // Product   KMS-Framework
 // File      KMS-A/Graph_Histogram.cpp
 
+// TEST COVERAGE  2023-09-06  KMS - Martin Dubois, P. Eng.
+
 #include "Component.h"
 
 // ===== Includes ===========================================================
@@ -17,19 +19,22 @@ namespace KMS
 
         Histogram::Histogram(double aMin, double aMax, unsigned int aZoneQty)
             : KMS::Stats::Histogram(aMin, aMax, aZoneQty)
-            , mDivY(0)
+            , mDivY(1)
         {}
 
         void Histogram::Prepare()
         {
             auto lSizeX_px = mBitmap.GetSizeX_px();
             auto lZoneQty = KMS::Stats::Histogram::GetZoneQty();
+            assert(0 < lSizeX_px);
+            assert(0 < lZoneQty);
 
             mStepX_px = lSizeX_px / lZoneQty;
             assert(0 < mStepX_px);
 
             mX0_px = (lSizeX_px - mStepX_px * lZoneQty) / 2;
             mX1_px = mX0_px + mStepX_px - 1 - mStepX_px / 4;
+            assert(mX0_px <= mX1_px);
 
             Clear();
         }
@@ -38,27 +43,34 @@ namespace KMS
 
         void Histogram::AddData(double aData)
         {
+            assert(0 < mDivY);
+
             KMS::Stats::Histogram::AddData(aData);
 
             auto lSizeY_px = mBitmap.GetSizeY_px();
             auto lZoneQty  = KMS::Stats::Histogram::GetZoneQty();
+            assert(0 < lSizeY_px);
+            assert(0 < lZoneQty);
 
             auto lZones = KMS::Stats::Histogram::GetZones();
+            assert(nullptr != lZones);
 
             for (unsigned int i = 0; i < lZoneQty; i++)
             {
-                auto y = lZones[i] / mDivY;
-                if (lSizeY_px <= y)
+                auto lY_px = lZones[i] / mDivY;
+                if (lSizeY_px <= lY_px)
                 {
+                    mDivY++;
                     Clear();
                     Draw();
                     break;
                 }
 
-                auto x0 = mX0_px + mStepX_px * i;
-                auto x1 = mX1_px + mStepX_px * i;
+                auto lX0_px = mX0_px + mStepX_px * i;
+                auto lX1_px = mX1_px + mStepX_px * i;
+                assert(lX0_px <= lX1_px);
 
-                mBitmap.SetLine(Point(x0, y), Point(x1, y), Color::WHITE);
+                mBitmap.SetLine(Point(lX0_px, lY_px), Point(lX1_px, lY_px), Color::WHITE);
             }
         }
 
@@ -66,7 +78,7 @@ namespace KMS
         {
             KMS::Stats::Histogram::Reset();
 
-            mDivY = 0;
+            mDivY = 1;
 
             Clear();
         }
@@ -81,18 +93,21 @@ namespace KMS
 
         void Histogram::Draw()
         {
+            assert(0 < mDivY);
+
             auto lZoneQty = KMS::Stats::Histogram::GetZoneQty();
 
             auto lZones = KMS::Stats::Histogram::GetZones();
+            assert(nullptr != lZones);
 
             for (unsigned int i = 0; i < lZoneQty; i++)
             {
-                auto y = lZones[i] / mDivY;
+                auto lY_px = lZones[i] / mDivY;
 
-                auto x0 = mX0_px + mStepX_px * i;
-                auto x1 = mX1_px + mStepX_px * i;
+                auto lX0_px = mX0_px + mStepX_px * i;
+                auto lX1_px = mX1_px + mStepX_px * i;
 
-                mBitmap.SetLine(Point(x0, 0), Point(x1, y), Color::WHITE);
+                mBitmap.SetLine(Point(lX0_px, 0), Point(lX1_px, lY_px), Color::WHITE);
             }
         }
 
