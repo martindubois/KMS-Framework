929b65a1d2768624621b65940cefd263f600b3e2
Martin Dubois <mdubois@kms-quebec.com>
2023-11-06 15:24:21 -0500

    Change version number for 1.1.0-dev

--- Common/Version.h
+++ Common/Version.h
@@ -15,8 +15,8 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  1,0,8,0
-#define VERSION_STR "1.0.8.0"
+#define VERSION_RC  1,1,0,0
+#define VERSION_STR "1.1.0.0"
 
 #ifdef __cplusplus
     KMS_VERSION("dev");
