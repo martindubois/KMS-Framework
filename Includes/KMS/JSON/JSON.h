42c41c21d5bd7a0308eddbd2fe0bb9b5c8e196bf
Martin Dubois <mdubois@kms-quebec.com>
2024-01-14 11:39:05 -0500

    1.1.4-dev Windows

--- Includes/KMS/JSON/JSON.h
+++ Includes/KMS/JSON/JSON.h
@@ -1,6 +1,6 @@
 
 // Author    KMS - Martin Dubois, P. Eng.
-// Copyright (C) 2022 KMS
+// Copyright (C) 2022-2024 KMS
 // License   http://www.apache.org/licenses/LICENSE-2.0
 // Product   KMS-Framework
 // File      Includes/KMS/JSON/JSON.h
@@ -17,8 +17,6 @@ namespace KMS
 
         extern unsigned int Decode(DI::Object*  aObject, const char* aIn , unsigned int aInSize_byte);
         extern unsigned int Decode(DI::Object** aObject, const char* aIn , unsigned int aInSize_byte);
-
-        extern unsigned int Decode_Dictionary(DI::Dictionary* aDictionary, const char* aIn, unsigned int aInSize_byte);
 
         extern unsigned int Encode(const DI::Object* aObject, char* aOut, unsigned int aOutSize_byte);
 
