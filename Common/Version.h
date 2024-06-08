b7b153cfc88c753820c694ccf29c6ac3b32c9c71
Martin Dubois <mdubois@kms-quebec.com>
2024-06-08 09:07:26 -0400

    1.2.2-dev Linux

--- Common/Version.h
+++ Common/Version.h
@@ -15,8 +15,8 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  1,2,1,0
-#define VERSION_STR "1.2.1.0"
+#define VERSION_RC  1,2,2,0
+#define VERSION_STR "1.2.2.0"
 
 #ifdef __cplusplus
     KMS_VERSION("dev");
