c4f81a933ec4fba3b837cc832d4f4c09528b46cd
Martin Dubois <mdubois@kms-quebec.com>
2024-09-09 09:22:24 -0400

    1.2.7-stm32h7

--- Common/Version.h
+++ Common/Version.h
@@ -15,8 +15,8 @@
 // Constants
 // //////////////////////////////////////////////////////////////////////////
 
-#define VERSION_RC  1,2,6,0
-#define VERSION_STR "1.2.6.0"
+#define VERSION_RC  1,2,7,0
+#define VERSION_STR "1.2.7.0"
 
 #ifdef __cplusplus
     KMS_VERSION("stm32h7");
