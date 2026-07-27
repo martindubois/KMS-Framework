f360ebded949b9a2850f3aaeb348ae35a7b4bbe6
Martin Dubois <mdubois@kms-quebec.com>
2026-07-27 09:37:29 -0400

    DI2 use offsetof

--- KMS-A/Build_Editor_DI2.cpp
+++ KMS-A/Build_Editor_DI2.cpp
@@ -8,11 +8,13 @@
 #include "Component.h"
 
 // ===== Includes ===========================================================
-#include <KMS/Build/Editor_DI2.h>
+#include <KMS/Build/Editor.h>
 #include <KMS/DI2/String.h>
 #include <KMS/String.h>
 #include <KMS/String_DI2.h>
 #include <KMS/Version_DI2.h>
+
+#include <KMS/Build/Editor_DI2.h>
 
 // Constants
 // //////////////////////////////////////////////////////////////////////////
@@ -26,9 +28,9 @@ namespace KMS
 
         const DI2::Struct_Field Editor_Config_FIELDS[4] =
         {
-            { "Operations" ,                                          0, &StringList_ASCII_TYPE },
-            { "Version"    , sizeof(StringList_ASCII)                  , &Version_TYPE          },
-            { "VersionFile", sizeof(StringList_ASCII) + sizeof(Version), &VERSION_FILE_TYPE     },
+            { "Operations" , offsetof(Editor_Config, mOperations ), &StringList_ASCII_TYPE },
+            { "Version"    , offsetof(Editor_Config, mVersion    ), &Version_TYPE          },
+            { "VersionFile", offsetof(Editor_Config, mVersionFile), &VERSION_FILE_TYPE     },
 
             { nullptr, 0, nullptr }
         };
